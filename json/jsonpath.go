42d1e929889fbc0cac00472133b59b03898908ab
lengchuan <lishuijun@kaiyuan.net>
2019-10-23 15:22:36 +0800

    jsonpath.go 通过value 查询jsonpath

--- json/jsonpath.go
+++ json/jsonpath.go
@@ -59,7 +59,66 @@ func FindPathByKey(j interface{}, key string) (paths []string, err error) {
 }
 
 //通过value 查找到 json path
-func FindPathByValue(j interface{}, value interface{}) (path []string, err error) {
+func FindPathByValue(j interface{}, value interface{}) (paths []string, err error) {
+	var dumpJson func(j interface{}, value interface{}, rootpath string)
+	paths = make([]string, 1, 10)
+
+	//
+	dumpJson = func(j interface{}, value interface{}, rootpath string) {
+		//遍历map
+		iterMap := func(m map[string]interface{}, value interface{}) {
+
+			for k, v := range m {
+				tmpPath := rootpath
+				tmpPath = fmt.Sprintf("%v.%v", tmpPath, k)
+				if v == value {
+					paths = append(paths, tmpPath)
+				}
+
+				//继续遍历
+				dumpJson(v, value, tmpPath)
+			}
+		}
+
+		//遍历slice
+		iterSlice := func(s []interface{}, value interface{}) {
+			for i, v := range s {
+				tmpPath := rootpath
+				tmpPath = fmt.Sprintf("%v.%v", tmpPath, i)
+
+				switch v.(type) {
+
+				case []interface{}:
+					//继续遍历
+					dumpJson(v, value, tmpPath)
+				case map[string]interface{}:
+					//继续遍历
+					dumpJson(v, value, tmpPath)
+
+				default:
+					if v == value {
+						paths = append(paths, tmpPath)
+					}
+				}
+			}
+		}
+
+		//处理
+		switch v := j.(type) {
+		//map
+		case map[string]interface{}:
+			iterMap(v, value)
+
+		//slice
+		case []interface{}:
+			iterSlice(v, value)
+
+		default:
+
+		}
+	}
+
+	dumpJson(j, value, "root")
 
 	return
 }
@@ -107,7 +166,15 @@ func main() {
 
 	var paths []string
 	var err error
-	if paths, err = FindPathByKey(j, "fld1"); err != nil {
+	if paths, err = FindPathByKey(j, "aaa"); err != nil {
+		panic(err)
+	}
+
+	for _, v := range paths {
+		fmt.Println(v)
+	}
+
+	if paths, err = FindPathByValue(j, "test2"); err != nil {
 		panic(err)
 	}
 
