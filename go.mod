f8d5899b66cff4ad0e78a7692ecadc94378f7865
lengchuan <lishuijun@kaiyuan.net>
2019-11-08 15:25:28 +0800

    logrus hook

--- go.mod
+++ go.mod
@@ -6,8 +6,12 @@ require (
 	github.com/Unknwon/goconfig v0.0.0-20190425194916-3dba17dd7b9e
 	github.com/gin-gonic/gin v1.4.0
 	github.com/google/gopacket v1.1.17
+	github.com/lestrrat-go/file-rotatelogs v2.2.1-0.20190718000351-e0a88b0fe840+incompatible
+	github.com/lestrrat-go/strftime v1.0.0 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da
 	github.com/sirupsen/logrus v1.4.3-0.20191026113918-67a7fdcf741f
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	go.uber.org/zap v1.12.0 // indirect
+	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529
 	golang.org/x/sys v0.0.0-20190422165155-953cdadca894
 )
--- logrus/main.go
+++ logrus/main.go
@@ -1,18 +1,107 @@
 package main
 
 import (
+	"fmt"
+	"github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"time"
 )
+
+var logger = log.New()
 
 func main() {
 
+	test1()
+
+	test2()
+
+	test3()
+
+	test4()
+}
+
+func test1() {
+	fmt.Println("---------------------test 1--------------------------")
 	//init
 	log.SetFormatter(&log.TextFormatter{})
-	f, _ := os.Open("../go_study/logrus/log.log")
-	log.SetOutput(f)
+	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
+	//打印文件、行数
+	log.SetReportCaller(true)
 
 	log.Error("test log Error")
 	log.Info("test log Info")
+}
+
+func test2() {
+	fmt.Println("---------------------test 2--------------------------")
+	//init
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+	//打印文件、行数
+	log.SetReportCaller(true)
+
+	log.Error("test log Error")
+	log.Info("test log Info")
+}
+
+func test3() {
+	fmt.Println("---------------------test 3--------------------------")
+	logger.SetFormatter(&log.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05.000",
+		FullTimestamp:   true,
+	})
+	logger.SetOutput(os.Stdout)
+	logger.SetReportCaller(true)
+
+	logger.WithFields(log.Fields{
+		"name":  "lengchuan",
+		"email": "lishuijun1992@gmail.com",
+	}).Info("test logrus WithFields")
+}
+
+//hook
+func test4() {
+	fmt.Println("---------------------test 4--------------------------")
+	logger.AddHook(newLfsHook(nil, 1024))
+
+	for {
+		logger.Info("test file hook")
+	}
+}
+
+func newLfsHook(logLevel *string, maxRemainCnt uint) log.Hook {
+	writer, err := rotatelogs.New(
+		"log"+".%Y%m%d%H"+".log",
+		// WithLinkName为最新的日志建立软连接,以方便随着找到当前日志文件
+		rotatelogs.WithLinkName("log"),
+
+		// WithRotationTime设置日志分割的时间,这里设置为一小时分割一次
+		rotatelogs.WithRotationTime(time.Second*3),
+
+		// WithMaxAge和WithRotationCount二者只能设置一个,
+		// WithMaxAge设置文件清理前的最长保存时间,
+		// WithRotationCount设置文件清理前最多保存的个数.
+		//rotatelogs.WithMaxAge(time.Hour*24),
+		rotatelogs.WithRotationCount(maxRemainCnt),
+	)
+
+	if err != nil {
+		log.Errorf("config local file system for logger error: %v", err)
+	}
+
+	log.SetLevel(log.WarnLevel)
+	lfsHook := lfshook.NewHook(lfshook.WriterMap{
+		log.DebugLevel: writer,
+		log.InfoLevel:  writer,
+		log.WarnLevel:  writer,
+		log.ErrorLevel: writer,
+		log.FatalLevel: writer,
+		log.PanicLevel: writer,
+	}, &log.TextFormatter{DisableColors: true})
+
+	return lfsHook
 }
